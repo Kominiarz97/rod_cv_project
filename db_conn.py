bef05438962a7bc7c0608a2352902c2d28ab6421
Maciek Krawiec <34010885+Kominiarz97@users.noreply.github.com>
2020-12-19 21:23:21 +0100

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -15,7 +15,9 @@ def db_connect():
 def insert_report(conn, byte, gps, drone_id, rail_id, obstacle):
     cur = conn.cursor() #stworzenie kursora
     #wykonanie instrukcji insert
-    cur.execute('INSERT INTO public."Zgloszenia"("Zdjecie", "Data_zgloszenia", "Godz_zgloszenia", "Lokalizacja_gps","Zarejestrowane", "Dron", "Trasa", "Rodzaj_zagrozenia") VALUES (%s, %s, %s, %s, False, %s, %s, %s);',(psycopg2.Binary(byte), datetime.now().date(), datetime.now().time(), gps, drone_id, rail_id, obstacle))
+    cur.execute('INSERT INTO public."Zgloszenia"("Zdjecie", "Data_zgloszenia", "Godz_zgloszenia", "Lokalizacja_gps",'
+                '"Zarejestrowane", "Dron", "Trasa", "Rodzaj_zagrozenia") VALUES (%s, %s, %s, %s, False, %s, %s, %s);',
+                (psycopg2.Binary(byte), datetime.now().date(), datetime.now().time(), gps, drone_id, rail_id, obstacle))
     conn.commit()
     cur.close()
 
